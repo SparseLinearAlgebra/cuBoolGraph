8d560159b7947ccc93f99eb6c1de648599faf272
Kostya Oreshin <84198636+sevenbunu@users.noreply.github.com>
2025-11-13 08:39:00 +0300

    ci: Add style check to CI (#3)
    * ci: add clang-format job and file
    
    * style: changes according to clang-format
    
    * fix: add correct trigger branches name

--- include/regular_path_query.hpp
+++ include/regular_path_query.hpp
@@ -3,20 +3,20 @@
 #include <cubool/cubool.h>
 
 cuBool_Matrix regular_path_query_with_transposed(
-  // vector of sparse graph matrices for each label
-  const std::vector<cuBool_Matrix> &graph, const std::vector<cuBool_Index> &source_vertices,
-  // vector of sparse automaton matrices for each label
-  const std::vector<cuBool_Matrix> &automaton, const std::vector<cuBool_Index> &start_states,
-  // transposed matrices for graph and automaton
-  const std::vector<cuBool_Matrix> &graph_transposed,
-  const std::vector<cuBool_Matrix> &automaton_transposed,
+        // vector of sparse graph matrices for each label
+        const std::vector<cuBool_Matrix>& graph, const std::vector<cuBool_Index>& source_vertices,
+        // vector of sparse automaton matrices for each label
+        const std::vector<cuBool_Matrix>& automaton, const std::vector<cuBool_Index>& start_states,
+        // transposed matrices for graph and automaton
+        const std::vector<cuBool_Matrix>& graph_transposed,
+        const std::vector<cuBool_Matrix>& automaton_transposed,
 
-  const std::vector<bool> &inversed_labels = {}, bool all_labels_are_inversed = false);
+        const std::vector<bool>& inversed_labels = {}, bool all_labels_are_inversed = false);
 
 cuBool_Matrix regular_path_query(
-  // vector of sparse graph matrices for each label
-  const std::vector<cuBool_Matrix> &graph, const std::vector<cuBool_Index> &source_vertices,
-  // vector of sparse automaton matrices for each label
-  const std::vector<cuBool_Matrix> &automaton, const std::vector<cuBool_Index> &start_states,
+        // vector of sparse graph matrices for each label
+        const std::vector<cuBool_Matrix>& graph, const std::vector<cuBool_Index>& source_vertices,
+        // vector of sparse automaton matrices for each label
+        const std::vector<cuBool_Matrix>& automaton, const std::vector<cuBool_Index>& start_states,
 
-  const std::vector<bool> &inversed_labels = {}, bool all_labels_are_inversed = false);
+        const std::vector<bool>& inversed_labels = {}, bool all_labels_are_inversed = false);
